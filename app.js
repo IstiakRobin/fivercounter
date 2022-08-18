ae94d877b9dfaa55be7617935469186306b7446d
Istiak Robin <72946671+IstiakRobin@users.noreply.github.com>
2022-08-19 01:40:51 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
 const fiverr = document.getElementById('fiverr');
-const counter = document.getElementById('counter');
+const counter = document.querySelector('#counter');
+const alarm_tone = document.querySelector('#alarmtone')
 
 fiverr.onsubmit = (e)=>{
 e.preventDefault()
 const formdata= new FormData(e.target)
-const alldata= Object.fromEntries(formdata.entries());
 const {date,time}= Object.fromEntries(formdata.entries());
 
 
+let count = setInterval(
 
-let startdate = Date.now();
+    function(){
+        let startTime = Date.now();
+        let endDate = new Date(date+' '+time);
+        let orderTime = Math.floor(Math.abs(endDate.getTime() - startTime));
 
-let count = setInterval(()=>{
+        let totalsec = Math.floor(orderTime/1000);
+        let totalmin = Math.floor(totalsec/60);
+        let totalhour = Math.floor(totalmin/60);
+        let totalday = Math.floor(totalhour/24);
 
-let enddate = new Date(date +' '+ time);
-let ordertime = Math.floor(Math.abs(enddate.getTime() - startdate));
-
-
-let sec = Math.floor(ordertime/1000);
-let min = Math.floor(sec/60);
-let hour = Math.floor(min/60);
-let day = Math.floor(hour/24);
-
-if(sec == 0)
-{
-    clearInterval(count);
-   
-}
-
-let totalhour =hour - (day * 24);
-let totalmin =min - (day * 24 * 60) - (hour * 60);
-let totalsec =sec - (day* 24 * 60 *60) - (hour * 60 *60) - (min * 60);
-counter.innerHTML = `<h4>${day} days : ${totalhour} hour : ${totalmin} minute: ${totalsec} second</h4>`
-},1000)
+        if(totalsec <= 0)
+        {
+            clearInterval(count);
+            alarm_tone.play();
 
 
+        }
 
+        let hour = totalhour - (totalday * 24)
+        let min = totalmin - (totalday * 24 * 60 ) - (hour  * 60)
+        let sec = totalsec - (totalday * 24 * 60  * 60) - (hour * 60 * 60) - (min *60)
 
-}
+        counter.innerHTML = `<h4> ${totalday} Days : ${hour} Hours : ${min} Minutes : ${sec} Seconds </h4>`;
+
+    },1000)}
