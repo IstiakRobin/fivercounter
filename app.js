5657d10d5f01d55d2937fdf19e745c3002a965a8
Istiak Robin <72946671+IstiakRobin@users.noreply.github.com>
2022-08-19 00:31:35 +0600

    Update app.js

--- app.js
+++ app.js
@@ -8,25 +8,11 @@ const alldata= Object.fromEntries(formdata.entries());
 const {date,time}= Object.fromEntries(formdata.entries());
 
 
+
 let startdate = Date.now();
-let enddate = new Date(date +' '+ time);
-let ordertime = Math.floor(Math.abs(enddate.getTime() - startdate));
-
-
-let sec = Math.floor(ordertime/1000);
-let min = Math.floor(sec/60);
-let hour = Math.floor(min/60);
-let day = Math.floor(hour/24);
-
-
-let totalhour =hour - (day*24);
-let totalmin =min - (day*24) - (hour*60);
-let totalsec =sec - (day*24) - (hour*60)-(sec*60);
-counter.innerHTML = `<h4>${day} days : ${hour} hour : ${min} minute: ${sec} second</h4>`
 
 let count = setInterval(()=>{
 
-let startdate = Date.now();
 let enddate = new Date(date +' '+ time);
 let ordertime = Math.floor(Math.abs(enddate.getTime() - startdate));
 
@@ -42,10 +28,10 @@ if(sec == 0)
    
 }
 
-let totalhour =hour - (day*24);
-let totalmin =min - (day*24 * 60) - (hour*60);
-let totalsec =sec - (day*24 * 60 *60) - (hour*60 *60)-(sec*60);
-counter.innerHTML = `<h4>${day} days : ${hour} hour : ${min} minute: ${sec} second</h4>`
+let totalhour =hour - (day * 24);
+let totalmin =min - (day * 24 * 60) - (hour * 60);
+let totalsec =sec - (day* 24 * 60 *60) - (hour * 60 *60) - (min * 60);
+counter.innerHTML = `<h4>${day} days : ${totalhour} hour : ${totalmin} minute: ${totalsec} second</h4>`
 },1000)
 
 
